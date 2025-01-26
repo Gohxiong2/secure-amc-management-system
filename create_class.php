7f1cb33c8eb2ce9b96b6d0e1103a70ca411a513c
xrtyu <glendonongsengyu@gmail.com>
2025-01-26 19:07:06 +0800

    lols3

--- create_class.php
+++ create_class.php
@@ -10,44 +10,72 @@
                 <label for='name'>
                     Class Name:
                 </label>
-                <input type='text' id='name' name='name' required>
+                <input type='text' name='name' required pattern="[a-zA-Z0-9\s]+">
             </div>
             <div>
                 <label for='semester'>
                     Semester:
                 </label>
-                <input type='text' id='semester' name='semester' required>
+                <input type='text' name='semester' required pattern="[a-zA-Z0-9\s]+">
             </div>
             <div>
                 <label for='start_date'>
                     Start date:
                 </label>
-                <input type='date' id='start_date' name='start_date' required>
+                <input type='date' name='start_date' required>
             </div>
             <div>
                 <label for='end_date'>
                     End Date:
                 </label>
-                <input type='date' id='end_date' name='end_date' required>
+                <input type='date' name='end_date' required>
             </div>
-            <button type='submit' name='create'></button>
+            <button type='submit' name='create'>Create</button>
         </form>
         <?php
             require 'db_connect.php';
-            $conn = db_connect();
             if (isset($_POST['create'])) {
-                $name = mysqli_real_escape_string($conn, $_POST['name']);
-                $semester = mysqli_real_escape_string($conn, $_POST['semester']);
-                $start_date = mysqli_real_escape_string($conn, $_POST['start_date']);
-                $end_date = mysqli_real_escape_string($conn, $_POST['end_date']);
+                $name = htmlspecialchars(trim($_POST['name'], ENT_QUOTES));
+                $semester = htmlspecialchars(trim($_POST['semester'], ENT_QUOTES));
+                $start_date = trim($_POST['start_date']);
+                $end_date = trim($_POST['end_date']);
 
-                $sql = "INSERT INTO classes (class_name, semester, start_date, end_date) VALUES ('$name', '$semester', '$start_date', '$end_date')";
+                $errors = 0
 
-                if ($conn->query($sql) === TRUE) {
-                    echo "Class created successfully!";
-                } 
-                else {
-                    echo "Error: " . $conn->error;
+                if (empty($name)){
+                    echo "Class name required!"
+                    $errors++
+                }
+                if ((!preg_match('/^[a-zA-Z0-9\s]+$/', $name))){
+                    echo "No special characters!"
+                    $errors++
+                }
+                if empty($semester){
+                    echo "Semester required!"
+                    $errors++
+                }
+                if empty($start_date){
+                    echo "Start date required!"
+                    $errors++
+                }
+                if empty($end_date){
+                    echo "End date required!"
+                    $errors++
+                }
+                if ($errors = 0){
+                    $stmt = $conn->prepare("insert into classes(class_id, class_name, semester, start_date, end_date) values
+                    (NULL,?,?,?,?)");
+                    $stmt->bind_param('ssss', $name, $semester, $start_date, $end_date );//bind the parameters
+                    $stmt->execute()
+                    if ($stmt->execute()) {
+                        echo "Class created successfully!";
+                    } 
+                    else {
+                        echo "Error: " . $stmt->error;
+                    }
+                }
+                else{
+                    echo "You have $errors errors."
                 }
             }
         ?>
